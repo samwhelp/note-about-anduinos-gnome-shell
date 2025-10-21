47e21e2f35b156a8d56ed26f5953b5e31d2844b6
samwhelp <samwhelp@users.noreply.github.com>
2025-10-21 01:07:46 -0700

    fix

--- _demo/scripts/gnome-shell-keybind/gnome-shell-keybind.sh
+++ _demo/scripts/gnome-shell-keybind/gnome-shell-keybind.sh
@@ -228,13 +228,13 @@ mod_gnome_shell_config_for_keybind_custom () {
 
 	## ### Terminal
 	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/name "'Terminal'"
-	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/command "'kgx'"
+	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/command "'ptyxis --new-window'"
 	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/binding "'<Alt>Return'"
 
 
 	## ### Terminal-1
 	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal-1/name "'Terminal-1'"
-	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal-1/command "'kgx'"
+	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal-1/command "'ptyxis --new-window'"
 	dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal-1/binding "'<Shift><Alt>a'"
 
 
